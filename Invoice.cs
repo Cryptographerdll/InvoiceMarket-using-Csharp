dc8068a2b27f783cddc4e1732e8d4b7bd8b14b5a
khalilDup <85350190+khalilDup@users.noreply.github.com>
2021-09-22 19:37:57 +0100

    Add files via upload

---
+++ Invoice.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace InvoiceMarket
+{
+    class Invoice
+    {
+        // A hardware store invoice for a client .
+
+        // public variables :
+        public string Partnumber { get; set; }
+        public string descriptions { get; set; }
+
+        // privates variable :
+        private int ItemQuantity;
+        private decimal pricePerItem;
+
+        public Invoice(string partnumber, string descriptions, int itemQuantity, decimal pricePerItem)
+        {
+            Partnumber = partnumber;
+            this.descriptions = descriptions;
+            qty = ItemQuantity;
+            Price = pricePerItem;
+
+        }
+
+        public int qty
+        {
+            get { return ItemQuantity; }
+            set
+            {
+                if (value > 0)
+                    ItemQuantity = value;
+                else
+                    ItemQuantity = 1;
+            }
+        }
+
+        public decimal Price
+        {
+            get { return pricePerItem; }
+            set
+            {
+                if (value > 0)
+                    pricePerItem = value;
+                else
+                    pricePerItem = 1;
+            }
+        }
+
+        public decimal GetInvoiceAmount()
+        {
+            return Price * qty;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace InvoiceMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num, des;
+            int qty;
+            decimal prc;
+
+            Console.WriteLine("please sir enter a part of number \n");
+            num = Console.ReadLine();
+
+            Console.WriteLine("please sir enter the item description \n");
+            des = Console.ReadLine();
+
+            Console.WriteLine("please enter the quantity \n");
+            qty = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("please enter the price \n");
+            prc = Convert.ToDecimal(Console.ReadLine());
+
+            Invoice invoice = new Invoice(num, des, qty, prc);
+
+            Console.Write("Your order : \n");
+            Console.Write("Part Number : {0}\n", invoice.Partnumber);
+            Console.Write("Part description : {0}\n", invoice.descriptions);
+            Console.Write("Your quantity : {0}\n", invoice.qty);
+            Console.Write("Your price : {0}\n", invoice.Price);
+
+            Console.Write("Your total is : {0} $\n", invoice.GetInvoiceAmount());
+        }
+    }
+}
